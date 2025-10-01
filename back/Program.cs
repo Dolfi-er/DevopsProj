d5164d65b0bc97913d12fe6c87191802f5b14100
Dolfi-er <dolgovasofia@yandex.ru>
2025-10-01 13:59:45 +0300

    fftf

--- back/Program.cs
+++ back/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
+//mvrkvn
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
